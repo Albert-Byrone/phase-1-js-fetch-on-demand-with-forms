eddcbc3733ce832afd125d8176e66f9108040d0c
Albert Byrone <albertbyrone1677@gmail.com>
2022-03-03 16:50:34 +0300

    commit tree fb2fd89035cc14388c8c7910b13577753607c885

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        fetch('http://localhost:3000/movies')
+        .then(response => response.json())
+        .then(data => {
+        console.log(data);
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      // LOG: (3) [{…}, {…}, {…}]
+    });
+
+    })
+
   
 }
 
